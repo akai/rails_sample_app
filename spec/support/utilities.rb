fd5952ada8fb2930f23c183d3970c1ccdf80b223
akai <akainice@gmail.com>
2013-01-17 23:28:22 +0800

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,7 +1,19 @@
 include ApplicationHelper
 
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
 RSpec::Matchers::define :have_title do |text|
   match do |page|
     Capybara.string(page.body).has_selector?('title', text: text)
+  end
+end
+
+RSpec::Matchers.define :have_error_messages do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
   end
 end
